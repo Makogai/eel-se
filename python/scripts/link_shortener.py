9e27e3c6c85f327b535eb4a7a828b0191c7409f0
Marko <mare.sampbn@gmail.com>
2021-06-17 11:27:02 +0200

    Updated URL Shortener, added adfly api

--- python/scripts/link_shortener.py
+++ python/scripts/link_shortener.py
@@ -7,7 +7,10 @@ def get_url(link):
     tinyurl = s.tinyurl.short(urls)
     dagd = s.dagd.short(urls)
     osdb = s.osdb.short(urls)
+
+    a = pyshorteners.Shortener(api_key="f8d435c7a95e278285c32d1f7e4f92a4", user_id="25486205", domain="q.gs")
+    adfly = a.adfly.short(urls)
     
-    short_url = {'tinyurl': tinyurl, 'dagd': dagd, 'osdb': osdb}
+    short_url = {'tinyurl': tinyurl, 'dagd': dagd, 'osdb': osdb, 'adfly': adfly}
 
     return short_url
