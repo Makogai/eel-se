173f1448100af2643eb7e11a5d9145f560940324
Marko <mare.sampbn@gmail.com>
2021-06-30 16:59:02 +0200

    fixed snake

--- python/scripts/images.py
+++ python/scripts/images.py
@@ -1,21 +1,17 @@
-# import required modules
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter.filedialog import askopenfilename,asksaveasfilename
 from PIL import Image, ImageTk, ImageFilter, ImageEnhance, ImageOps
 import os
-# contrast border thumbnail 
 root = Tk()
 root.title("Simple Photo Editor")
 root.geometry("640x640")
-# create functions
 def selected():
     global img_path, img
     img_path = filedialog.askopenfilename(initialdir=os.getcwd()) 
     img = Image.open(img_path)
     img.thumbnail((350, 350))
-    #imgg = img.filter(ImageFilter.BoxBlur(0))
     img1 = ImageTk.PhotoImage(img)
     canvas2.create_image(300, 210, image=img1)
     canvas2.image=img1                                                                                                                                                                                                                
